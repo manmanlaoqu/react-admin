dc4e10572fa72975544f9aeccf907c76c38aecb5
manmanlaoqu <37039911+manmanlaoqu@users.noreply.github.com>
2019-11-03 19:58:22 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 
 export default class Versions {
-    static SOCKET_SERVER = '测试地址'
+    static SOCKET_SERVER = 'wss://测试地址'
     static API = 'http://localhost:8800';
     static VERSION = 'replace-version';
 }
