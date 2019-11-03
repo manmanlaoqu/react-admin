2404636b2a340db591771179e7691f3fe1da06a0
manmanlaoqu <37039911+manmanlaoqu@users.noreply.github.com>
2019-11-03 19:59:28 +0800

    Update config.pro.js

--- config/config.pro.js
+++ config/config.pro.js
@@ -1,6 +1,6 @@
 
 export default class Versions{
-    static SOCKET_SERVER = 'wss://test-im.utrailer.cn'
+    static SOCKET_SERVER = 'wss://测试地址'
     static VERSION = 'replace-version';
-    static API = 'https://ebs.utrailer.cn:8800';
+    static API = '正式地址:8800';
 }
