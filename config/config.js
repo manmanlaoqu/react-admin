6ed368df5e97b91302dc573046c5f211d19b9585
manmanlaoqu <37039911+manmanlaoqu@users.noreply.github.com>
2019-11-03 19:57:38 +0800

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 
 export default class Versions {
-    static SOCKET_SERVER = 'wss://test-im.utrailer.cn'
+    static SOCKET_SERVER = '测试地址'
     static API = 'http://localhost:8800';
     static VERSION = 'replace-version';
 }
